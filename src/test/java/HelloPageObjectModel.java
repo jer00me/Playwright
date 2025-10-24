9ace871a8407404d7bbd7613166febfd37bc6fc7
Jer00me <jerome.kurth@gmail.com>
2025-10-24 12:14:06 +0200

    Added HelloPageObjectModel

---
+++ src/test/java/HelloPageObjectModel.java
@@ -0,0 +1,9 @@
+import com.microsoft.playwright.Page;
+
+public class HelloPageObjectModel extends AbstractPageObjectModel {
+
+    public HelloPageObjectModel(Page page) {
+        super(page);
+    }
+
+}
