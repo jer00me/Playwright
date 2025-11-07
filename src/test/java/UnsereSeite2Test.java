05ec42f34e838d89d84c465ead670de4cc9c3e93
Jer00me <jerome.kurth@gmail.com>
2025-11-07 11:10:10 +0100

    Added Comments and DisplayNames to tests in US2Test

--- src/test/java/UnsereSeite2Test.java
+++ src/test/java/UnsereSeite2Test.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,12 +14,36 @@ class UnsereSeite2Test extends PlaywrightAbstractTester{
         us2.open();
     }
 
+    /**
+     * WHEN I open the webpage "unsere_seite2.html" in a Browser
+     * THEN the title is "Unsere Seite 2"
+     */
     @Test
+    @DisplayName("minor: title text of page")
     void testPageTitle() {
-        assertEquals("Unsere Seite 2", us2.title());
+        String expectedTitle = "Unsere Seite 2";
+        String actualTitle = us2.title();
+        assertEquals(expectedTitle, actualTitle);
     }
 
+    /**
+     * WHEN I open the webpage "unsere_seite2.html" in a Browser
+     * THEN the header is "Unsere Seite 2"
+     */
     @Test
+    @DisplayName("minor: header text of page")
+    void testHeader() {
+        String expectedHeader = "Unsere Seite 2";
+        String actualHeader = us2.getHeader();
+        assertEquals(expectedHeader, actualHeader);
+    }
+
+    /**
+     * WHEN I select the radio option "Ja"
+     * THEN the feedback is "Freut uns zu hören!"
+     */
+    @Test
+    @DisplayName("major: right feedback text when yes selected")
     void testFeedbackYes() {
         us2.clickYes();
         String expectedFeedback = "Freut uns zu hören!";
@@ -26,7 +51,12 @@ class UnsereSeite2Test extends PlaywrightAbstractTester{
         assertEquals(expectedFeedback, actualFeedback);
     }
 
+    /**
+     * WHEN I select the radio option "Nein"
+     * THEN the feedback is "Schade."
+     */
     @Test
+    @DisplayName("major: right feedback text when no selected")
     void testFeedbackNo() {
         us2.clickNo();
         String expectedFeedback = "Schade.";
