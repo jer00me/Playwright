d572d1e47f75d5a78cd78d4d4de38f0f96c2b478
Louis19200 <louischanteloup19@gmail.com>
2025-11-07 11:38:53 +0100

    Updated UnsereSeiteTest.java, added DisplayName and comments

--- src/test/java/UnsereSeiteTest.java
+++ src/test/java/UnsereSeiteTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class UnsereSeiteTest extends PlaywrightAbstractTester {
 
