701bb0d5148e5c4cf0ce3e097a90adc7ddc38bf2
Jer00me <jerome.kurth@gmail.com>
2025-11-07 10:07:40 +0100

    Removed useless classes

--- src/test/java/HelloPageObjectModel.java
+++
@@ -1,9 +0,0 @@
-import com.microsoft.playwright.Page;
-
-public class HelloPageObjectModel extends AbstractPageObjectModel {
-
-    public HelloPageObjectModel(Page page) {
-        super(page);
-    }
-
-}
